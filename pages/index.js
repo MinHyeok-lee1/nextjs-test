9873b9b971a3b6e9c7479bc15865d3b6f8e19d48
minhyeok <minhyeok.lee1@gmail.com>
2021-12-30 13:17:04 +0900

    test_comit2

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home(){
     return (
         <div>
             <NavBar />
-            <h1>Hello</h1>
+            <h1>Hello123</h1>
             <style></style>
         </div>
     )
